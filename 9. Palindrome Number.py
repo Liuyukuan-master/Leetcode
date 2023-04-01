89e2cc58ca4d5458d951c4a007b21ff28f76997c
Liuyukuan-master <127182526+Liuyukuan-master@users.noreply.github.com>
2023-04-01 10:36:54 +0800

    Create 9. Palindrome Number.py

---
+++ 9. Palindrome Number.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        return str(x) == str(x)[::-1]
+
+作者：qnjmdt2DDT
+链接：https://leetcode.cn/problems/palindrome-number/solution/by-qnjmdt2ddt-zo53/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
